6aae928b7cff62e715d10f3f456c99dac59d9eb0
Nick-Flammer <66448953+Nick-Flammer-55@users.noreply.github.com>
2025-09-22 22:13:01 -0400

    Developed first map where the pins of each park are placed, will likely customize it differently in the future.

---
+++ back-end/README.md
@@ -0,0 +1,11 @@
+To set up the back end, follow these steps:
+
+    -   First clone this repository using whichever cloning option works best
+
+    -   Then, type the following into the terminal: "python -m venv"
+
+    -   After which, type "source .venv/Scripts/activate" in bash terminal
+
+        -   Currently, the terminal should have a "(.venv)" above the header
+
+    -   Finally, type "pip install -r requirement.txt"
---
+++ back-end/map.py
@@ -0,0 +1,44 @@
+"""
+version 1.0
+
+@Date: 09/18/2025
+"""
+import folium
+import csv
+import webbrowser
+
+# Starts the map at the center of the mainland U.S.
+main_map = folium.Map(
+    location=(39.833333, -98.583333), # Where the map will be positioned
+    zoom_start=3, max_bounds=True, min_zoom=3, max_lat=75, min_lat=-20, max_lon=-60, min_lon=-180, # The bounds of the map
+    )
+
+with open('npdata.csv', newline='', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    park_list = list(reader)[1:] # Skips the header
+
+"""
+Park Item Key:
+[
+(0. Name), 
+(1. State/Territory full name), 
+(2. State/Territory abbreviation), 
+(3. Latitude), 
+(4. Longitude),
+(5. Year Established)
+]
+"""
+
+# Adds markers for each US National Park
+def build_map():
+    for park in park_list:
+        folium.Marker(
+            location=[float(park[3]), float(park[4])], 
+            tooltip=park[0]
+        ).add_to(main_map)
+
+    # Saves the completed map as an HTML File
+    main_map.save("nps_map.html")
+
+if __name__ == "__main__":
+    build_map()
