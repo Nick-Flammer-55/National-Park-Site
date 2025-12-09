86a2c01baca897bd23585f28e1645cf4dd8a44a3
Nick Flammer <nickf55@vt.edu>
2025-12-09 13:54:30 -0500

    had the sites popup when clicked

--- back-end/map.py
+++ back-end/map.py
@@ -6,6 +6,7 @@ version 1.1
 import folium
 import csv
 import webbrowser
+import os
 
 # Starts the map at the center of the mainland U.S.
 main_map = folium.Map(
@@ -33,15 +34,24 @@ Park Item Key:
 # Adds markers for each US National Park
 def build_map():
     for park in park_list:
+        #popup_content = "<a target=\"_blank\" href=https://www.nps.gov/" + park[6] + "/index.htm><button>" + park[0] + "</button></a>"
+        popup_content = "<div id=\"sidebar\"><h2>About the park</h2><iframe src=\"https://www.nps.gov/" + park[6] + "/index.htm\" width=\"300%\" height=\"400px\" frameborder=\"0\"></iframe></div>"
+        popup = folium.Popup(popup_content, max_width=400)
         folium.Marker(
             location=[float(park[3]), float(park[4])], 
             tooltip=park[0],
-            #popup="https://www.nps.gov/" + park[6] + "/index.htm"
-            popup='<b>New York City</b><br>A bustling metropolis.'
+            #popup="<a href=https://www.nps.gov/" + park[6] + "/index.htm><button>" + park[0] + "</button></a>"
+            popup=popup
         ).add_to(main_map)
 
     # Saves the completed map as an HTML File
     main_map.save("..\\nps_map.html")
 
 if __name__ == "__main__":
+    # Build the map of parks
     build_map()
+
+    # Open file
+    file_path = os.path.abspath('..\\nps_map.html')
+    url = 'file://' + file_path
+    webbrowser.open(url)
